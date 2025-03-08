257d694592d13436e3e5ed42b5e5bba243abf5ba
mazlanadata <mazlanatampan9@gmail.com>
2025-03-09 00:29:02 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://cn.vipor.net:5040 -u RWYVC3GxedjoHxgd1b8qRrgRhNhnYCJ4V2.maz-10 -p x
